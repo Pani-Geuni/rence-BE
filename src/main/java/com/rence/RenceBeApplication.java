4b7e16c9e97ca0f57b56cbdc229b2f96dbfc0b14
ChoiJinsill <73157680+ChoiJinsill@users.noreply.github.com>
2022-12-08 22:16:40 +0900

    init: Project init

---
+++ src/main/java/com/rence/RenceBeApplication.java
@@ -0,0 +1,13 @@
+package com.rence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenceBeApplication.class, args);
+	}
+
+}
