bda38f95fb8ab1ca6bf1a949e00c88f9f4f684bd
Choi <choetruth44@gmail.com>
2022-12-12 09:34:01 +0900

    [refactor] modelmapper, officereservevo
    modelmapper, officereservevo

--- src/main/java/com/rence/config/PropertyConfig.java
+++ src/main/java/com/rence/config/PropertyConfig.java
@@ -1,5 +1,7 @@
 package com.rence.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -9,5 +11,10 @@ import org.springframework.context.annotation.PropertySources;
 	@PropertySource("classpath:properties/env.properties")
 })
 public class PropertyConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 	
 }
